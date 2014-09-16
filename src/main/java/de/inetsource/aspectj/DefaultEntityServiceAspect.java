3ed4b15b4c03d1d8b36d13993eb6808e9a334bb3
Joerg Wiesmann <>
2014-09-16 10:14:36 +0200

    finally its working

--- src/main/java/de/inetsource/aspectj/DefaultEntityServiceAspect.java
+++ src/main/java/de/inetsource/aspectj/DefaultEntityServiceAspect.java
@@ -4,11 +4,13 @@ import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Jörg Wiesmann
  */
 @Aspect
+@Component
 public class DefaultEntityServiceAspect {
 
     private final Logger logger = Logger.getLogger(DefaultEntityServiceAspect.class);
